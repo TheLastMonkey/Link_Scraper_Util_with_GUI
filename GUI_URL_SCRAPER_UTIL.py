60b3e23a4a00a1115f5d79bf91c074f0983fae77
TXJTbWl0aA== <32136907+TheLastMonkey@users.noreply.github.com>
2019-06-13 12:28:39 -0500

    1.3 UPDATE! Added User Data option

--- GUI_URL_SCRAPER_UTIL.py
+++ GUI_URL_SCRAPER_UTIL.py
@@ -9,7 +9,7 @@ layout=[[sg.Text('Input File Path or Url')],
         [sg.Text("URL:                   "),sg.Input("",key="_URL_")],
         [sg.Text("Add regex:           "),sg.Input("",key="_RX_")],
         [sg.Text("Output File Name:"),sg.Input("",key="_OUTputFILE_")],
-        [sg.Checkbox("Headless",key="_HEAD_",default=True),sg.Text("Wait in secs: "),sg.Input("0",key="_WAIT_",size=[5,1])],
+        [sg.Checkbox("Headless",key="_HEAD_",default=True),sg.Checkbox("Keep User Data - Logins, Cookies, Etc ",key="_USER_Data_"),sg.Text("Wait in Secs: "),sg.Input("0",key="_WAIT_",size=[5,1])],
         [sg.OK("OK"), sg.Cancel(),sg.Text(" "*60),sg.Text("All fields are optional.")] ]
 
 window = sg.Window('Scraper Setup').Layout(layout)
@@ -32,6 +32,9 @@ try:
 except Exception as e:
     print(e)
     wait_time=0
+#########################################
+user_DATA=values["_USER_Data_"]
+
 
 ######################################### file or url
 if file_url=="":
@@ -58,7 +61,11 @@ try:
     else:
         options.add_argument("--headless")
     options.add_argument("--window-size=192x108")
-    #options.add_argument("user-data-dir=selenium")
+    if user_DATA == False:
+        pass
+    else:
+        options.add_argument("user-data-dir=selenium")
+    ###################################
     # driver setup
     driver = webdriver.Chrome(options=options, executable_path="./sel_driver/chromedriver")
     driver.set_page_load_timeout(60)
